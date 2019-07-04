86eee019b5d46c8f8902be59d4cb463ea26e1eea
Zion Perez <zionperez@gmail.com>
2019-07-04 17:53:31 +0900

    first commit

---
+++ Assets/GyroControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GyroControl : MonoBehaviour
+{
+    public TextMeshProUGUI gyroText;
+    public TextMeshProUGUI accelerometerText;
+    public GameObject gyroCube;
+    public GameObject accelerometerCube;
+    public float scaleFactor;
+
+    private bool gyroEnabled;
+    private Gyroscope gyro;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gyroEnabled = EnableGyro();
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            return true;
+        }
+        return false;
+    }
+
+    private void Update()
+    {
+        gyroText.text = "Gyro: " + gyro.attitude.ToString();
+        gyroCube.transform.position = new Vector3(gyro.attitude.x * scaleFactor, gyro.attitude.y * scaleFactor, gyroCube.transform.position.z);
+
+        accelerometerText.text = "Accel: " + Input.acceleration.ToString();
+        accelerometerCube.transform.position = new Vector3(Input.acceleration.x * scaleFactor, Input.acceleration.y * scaleFactor, accelerometerCube.transform.position.z);
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Unity Gyroscope vs Accelerometer Tester
+
+Compare Gyroscope and Accelerometer input within Unity with this sample project. The green "G" square is the gyroscope input. The red "A" square is the accelerometer input. They both move along the x-axis and y-axis according to their respective values. The raw values are also displayed on the bototm left of the screen.
+
+![alt text](https://imgur.com/a/H9Zs3CP)
